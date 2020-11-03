a508aff0f9719d8682e115d26fa8abd9aff64adc
mhx <shoaky159@gmail.com>
2020-11-03 12:28:18 +0800

    fix(fluid): 流体提取
    - 流体提取时机不当

--- src/recipe/Progress.lua
+++ src/recipe/Progress.lua
@@ -20,37 +20,32 @@ end
 
 function _M:start()
     print(self.processItem.nickname .. " processing")
-    --xpcall(function ()
-        local fluidSlot = 0
-        for _, v in pairs(self.items) do
-            local type = v.type
-            print("trans item:" .. v[1] .. " type:".. type .. " amount:" .. v.amount )
-            if type == nil or type == "item" then
-                self:transRecipeItem(v)
-            elseif type == "molten" then
-                fluidSlot = fluidSlot + 1
-                self.suckSlot[fluidSlot] = v.amount * v.times
-                self:transRecipeMolten(v)
-            elseif type == "fluid" then
-                fluidSlot = fluidSlot + 1
-                self:transRecipeFluid(v, fluidSlot)
-            elseif type == "cell" then
-                fluidSlot = fluidSlot + 1
-                self.suckSlot[fluidSlot] = v.amount * 1000
-                self:transRecipeCell(v, fluidSlot)
-            end
+    local fluidSlot = 0
+    for _, v in pairs(self.items) do
+        local type = v.type
+        print("trans item:" .. v[1] .. " type:" .. type .. " amount:" .. v.amount)
+        if type == nil or type == "item" then
+            self:transRecipeItem(v)
+        elseif type == "molten" then
+            fluidSlot = fluidSlot + 1
+            self.suckSlot[fluidSlot] = v.amount * v.times
+            self:transRecipeMolten(v)
+        elseif type == "fluid" then
+            fluidSlot = fluidSlot + 1
+            self:transRecipeFluid(v, fluidSlot)
+        elseif type == "cell" then
+            fluidSlot = fluidSlot + 1
+            self.suckSlot[fluidSlot] = v.amount * 1000
+            self:transRecipeCell(v, fluidSlot)
         end
-        self:waitInTank(fluidSlot)
-        self:suckTankFluid()
-        self:toItemInputBus()
-        --因为在花园时间时间获取不了
-        --local cost = os.time() - self.startTime
-        --print("all transported cost:" .. cost .. ", waiting for assembly line crafting")
-        print("all transported, waiting for assembly line crafting")
-        --TODO 如果同时合成多个有问题,在这里检测装配线红石信号 直到熄灭
-    --end, function ()
-    --    computer.beep(1000, 5)
-    --end)
+    end
+    self:waiting4Fluid(fluidSlot)
+    self:suckTankFluid()
+    self:toItemInputBus()
+    --因为在花园时间时间获取不了
+    --local cost = os.time() - self.startTime
+    --print("all transported cost:" .. cost .. ", waiting for assembly line crafting")
+    print("all transported, waiting for assembly line crafting")
 end
 
 function _M:transRecipeItem(recipeItem)
@@ -83,20 +78,44 @@ function _M:toItemInputBus()
     for _, item in pairs(self.items) do
         local type = item.type
         if type == "item" or type == nil then
+            --TODO 检查输入总线当前物品数量,如果amount+当前数量大于64 暂停
             slot = slot + 1
             transport.transOutput(slot, item)
         end
     end
 end
 
-function _M:waitInTank(maxSlot)
-    --TODO 同时检测所有槽位 如果都等于才释放
+function _M:waiting4Fluid(maxSlot)
+    local coroutines = {}
     for i = 1, maxSlot do
-        local amount = transport.getTankFluid(i).amount
-        local need = self.suckSlot[i]
-        if need == amount then
+        local co = coroutine.create(_M:checkTank(i))
+        table.insert(coroutines, i, co)
+        coroutine.resume(co)
+    end
 
+    repeat
+        ::continue::
+        for i, v in pairs(coroutines) do
+            local status, value = coroutine.resume(v)
+            if value == false or not coroutine.status(v) == "dead" then
+                os.sleep(0.2)
+                goto continue
+            else
+                coroutines[i] = nil
+                print("slot:" .. i .. " done")
+            end
         end
+    until true
+end
+
+function _M:checkTank(slot)
+    while true do
+        local current = transport.getTankFluid(slot).amount
+        local need = self.suckSlot[slot]
+        if current == need then
+            return true
+        end
+        coroutine.yield(false)
     end
 end
 
--- src/test.lua
+++ src/test.lua
@@ -3,9 +3,9 @@
 --- Created by Admin.
 --- DateTime: 2020/10/26 18:40
 ---
-local pg = require("recipe/Progress")
-local matcher = require("recipe/matcher")
-local recipes = require("recipe/recipes")
+--local pg = require("recipe/Progress")
+--local matcher = require("recipe/matcher")
+--local recipes = require("recipe/recipes")
 --local reader = require("chest/reader")
 --print(pg)
 --local pgtest = pg.new("test")
@@ -14,18 +14,49 @@ local recipes = require("recipe/recipes")
 
 --local recipe = matcher.match(recipes)
 --print(recipe)
+local _M = {}
+function _M.check(slot, amount)
+    print("check slot:" .. slot .. " amount:" .. amount)
+    if amount then
 
-local testData = {
-    { name = "item1" },
-    { name = "item2" },
-    { name = "item3" },
-    { name = "item4" },
-    { name = "item5" },
-    { name = "item6" }
-}
+    end
+    error("121")
+end
 
-local recipe = matcher.match(testData)
-pg:new(recipe)
-pg:start()
-print("==========")
-print(recipe)
+function _M.sleep(n)
+    if n > 0 then os.execute("ping -n " .. tonumber(n + 1) .. " localhost > NUL") end
+end
+
+local coroutines = {}
+for i = 1, 4 do
+    local co = coroutine.create(function(slot, amount)
+        while true do
+            local ok = _M.check(slot, amount)
+            if ok then
+                return
+            end
+            coroutine.yield(false)
+        end
+    end)
+    table.insert(coroutines, i, co)
+end
+
+for i, v in pairs(coroutines) do
+    local status, value = coroutine.resume(v, i, i + 10)
+    if value ~= false then
+        --coroutines[i] = nil
+    end
+end
+
+repeat
+    ::continue::
+    for i, v in pairs(coroutines) do
+        local status, value = coroutine.resume(v)
+        if value == false or not coroutine.status(v) == "dead" then
+            print("sleep")
+            goto continue
+        else
+            coroutines[i] = nil
+        end
+    end
+until true
