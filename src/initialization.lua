ee96ddbddf7f2896389fcf34fcaa09a61515155f
shoaky <shoaky159>
2021-02-06 13:30:57 +0800

    update init

--- src/initialization.lua
+++ src/initialization.lua
@@ -3,7 +3,7 @@ local Init = {}
 local event = require("event")
 local file = io.open("./conf/config.lua", "r")
 local config = file:read("*a")
-file.close()
+file.close(self)
 
 local ci_flag = false
 local co_count = 1
@@ -38,29 +38,23 @@ function Init.check()
 end
 
 while true do
-    local id, _1, _2, _3 = event.pull("component_added")
-    print(id)
-    print(_1)
-    print(_2)
-    print(_3)
+    local id, address, cp_type = event.pull("component_added")
     if id then
         if not ci_flag then
-            config:gsub("${ci1}", "address")
+            config = config:gsub("${ci1}", address)
             ci_flag = true
         end
         if co_count < 13 then
-            config:gsub(string.format("${co%d}", co_count), "address")
+            config = config:gsub(string.format("${co%d}", co_count), address)
             co_count = co_count + 1
         elseif fi_count < 4 then
-            config:gsub(string.format(string.format("${fi%d}", fi_count), co_count), "address")
+            config = config:gsub(string.format(string.format("${fi%d}", fi_count), co_count), address)
             fi_count = fi_count + 1
         elseif fif_count < 4 then
-            config:gsub(string.format("${fif%d}", fif_count), "address")
+            config = config:gsub(string.format("${fif%d}", fif_count), address)
             fif_count = fif_count + 1
         end
-        print("replace:" .. replace)
-        --TODO find address var
-        config:gsub(replace, "address")
+        print(config)
     end
     os.sleep(1)
 end
