2e19b794e90f4aa662a3ecbf51e9b850d167e9ff
shoaky <shoaky159>
2021-02-06 21:00:12 +0800

    update init

--- src/initializer.lua
+++ src/initializer.lua
@@ -8,25 +8,53 @@ local config = file:read("*a")
 io.close(file)
 
 local ci_flag = false
-local co_count = 1
-local fi_count = 1
-local fif_count = 1
+local co_count = 0
+local fi_count = 0
+local fif_count = 0
+
+thread.create(function()
+    while true do
+        local id, address, cp_type = event.pull("component_added")
+        if id then
+            if cp_type == "transposer" then
+                if not ci_flag then
+                    config = config:gsub("${ci1}", address)
+                    ci_flag = true
+                elseif co_count <= 13 then
+                    config = config:gsub(string.format("${co%d}", co_count), address)
+                    co_count = co_count + 1
+                elseif fi_count <= 4 then
+                    config = config:gsub(string.format("${fi%d}", fi_count), address)
+                    fi_count = fi_count + 1
+                end
+            end
+
+            if cp_type == "me_interface" then
+                if fif_count <= 4 then
+                    config = config:gsub(string.format("${fif%d}", fif_count), address)
+                    fif_count = fif_count + 1
+                end
+            end
+            print(config)
+        end
+    end
+end)
 
 function Init.start()
     local timer = event.timer(2, Init.check, math.huge)
     print("started!")
     while true do
-        local id, _, x, y = event.pullMultiple("interrupted")
+        local id, _, _x, _y = event.pullMultiple("interrupted")
         if id == "interrupted" then
             print("interrupted cancel timer")
             event.cancel(timer)
-            break
+            os.exit(0)
         end
     end
 end
 
 function Init.check()
-    if ci_flag and co_count <= 13 and fi_count <= 4 and fif_count <= 4 then
+    if ci_flag and co_count >= 13 and fi_count >= 4 and fif_count >= 4 then
         local config_file = io.open("./conf/config.lua", "w+")
         print("write:" .. config)
         config_file:write(config)
@@ -34,34 +62,6 @@ function Init.check()
         computer.shutdown(true)
     end
 end
-
-thread.create(function()
-    local id, address, cp_type = coroutine.yield()
-    print(id)
-    if id then
-        if cp_type == "transposer" then
-            if not ci_flag then
-                config = config:gsub("${ci1}", address)
-                ci_flag = true
-            elseif co_count < 13 then
-                config = config:gsub(string.format("${co%d}", co_count), address)
-                co_count = co_count + 1
-            elseif fi_count < 4 then
-                config = config:gsub(string.format("${fi%d}", fi_count), address)
-                fi_count = fi_count + 1
-            end
-        end
-
-        if cp_type == "me_interface" then
-            if fif_count < 4 then
-                config = config:gsub(string.format("${fif%d}", fif_count), address)
-                fif_count = fif_count + 1
-            end
-        end
-        print(config)
-    end
-    event.pull("component_added")
-end)
 
 Init.start()
 
