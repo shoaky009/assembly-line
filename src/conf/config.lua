daa2b9bf2fcc953e1a0146bc07b11852c33b3d62
mhx <shoaky159@gmail.com>
2021-02-06 18:35:21 +0800

    rename

--- src/conf/config.lua
+++ src/conf/config.lua
@@ -6,28 +6,21 @@ local config = {
 local manager = require("manager")
 local sides = require("sides")
 
-config.chestInput.proxy = manager.proxy("${ci1}")
 --me interface chest
 config.chestInput.chestSourceSide = sides.north
 config.chestInput.chestOutputSide = sides.south
 --molten output side
 config.chestInput.moltenOutputSide = sides.west
 config.chestInput.checkInterval = 3
-
 --fluid source side
 config.fluidSourceSide = 0
 config.tankSourceSide = sides.west
+--transposer
+config.chestInput.proxy = manager.proxy("${ci1}")
 config.fluidInput[1] = manager.proxy("${fi1}")
 config.fluidInput[2] = manager.proxy("${fi2}")
 config.fluidInput[3] = manager.proxy("${fi3}")
 config.fluidInput[4] = manager.proxy("${fi4}")
-
---me fluid interfaces
-config.fluidInterface[1] = manager.proxy("${fif1}")
-config.fluidInterface[2]= manager.proxy("${fif2}")
-config.fluidInterface[3] = manager.proxy("${fif3}")
-config.fluidInterface[4] = manager.proxy("${fif4}")
-
 --ender chest
 config.chestOutput[1] = manager.proxy("${co1}")
 config.chestOutput[2] = manager.proxy("${co2}")
@@ -42,5 +35,10 @@ config.chestOutput[10] = manager.proxy("${co10}")
 config.chestOutput[11] = manager.proxy("${co11}")
 config.chestOutput[12] = manager.proxy("${co12}")
 config.chestOutput[13] = manager.proxy("${co13}")
+--me fluid interfaces
+config.fluidInterface[1] = manager.proxy("${fif1}")
+config.fluidInterface[2]= manager.proxy("${fif2}")
+config.fluidInterface[3] = manager.proxy("${fif3}")
+config.fluidInterface[4] = manager.proxy("${fif4}")
 
 return config
